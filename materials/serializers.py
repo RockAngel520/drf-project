d9f87c7bdcc40e5e03e8089d03d10eaf1a6e787e
Anton-Z <rockangel520@mail.ru>
2025-11-01 15:22:29 +0300

    complete homework 30.1

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from materials.models import Course, Lesson
 
@@ -13,3 +13,20 @@ class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+
+    lesson_count_in_course = SerializerMethodField()
+    lessons = SerializerMethodField()
+
+    def get_lesson_count_in_course(self, course):
+        return course.lesson_set.count()
+
+    def get_lessons(self, course):
+        lessons = Lesson.objects.filter(course=course)
+        return [lesson.name for lesson in lessons]
+
+    class Meta:
+        model = Course
+        fields = ("name", "description", "image", "lesson_count_in_course", "lessons")
